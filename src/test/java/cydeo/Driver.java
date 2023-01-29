e0e8aef7b82e03986b1df315a9cee85e5b03048f
mehmetCt <85017903+mehmetCt@users.noreply.github.com>
2023-01-29 17:39:19 -0500

    Update Driver.java

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -57,7 +57,7 @@ public class Driver {
                     }
                     break;
                 case "chrome":
-                    WebDriverManager.chromedriver().setup().browserVersion("103");
+                    WebDriverManager.chromedriver().browserVersion("103").setup();
                     driver = new ChromeDriver();
                     break;
                 case "chrome-headless":

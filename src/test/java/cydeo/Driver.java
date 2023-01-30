ec5352aab9e69a56bdf4cbbb6fdb53d1bd64bc52
mehmetCt <mehmet@cybertekschool.com>
2023-01-30 18:02:39 -0500

    Selenium Grid with Jenkins

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.234.84.104";
+                        String gridAddress = "34.201.52.112";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");

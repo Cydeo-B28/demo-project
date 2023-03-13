0e94f1e626a7103f654549af8323d1d51b3b2be5
cavama <114663867+cavama@users.noreply.github.com>
2023-03-13 08:53:54 -0400

    Update StepDefs.java

--- src/test/java/cydeo/StepDefs.java
+++ src/test/java/cydeo/StepDefs.java
@@ -31,7 +31,7 @@ public class StepDefs { // thi sis a test 7;30 am // 7:43 am again // 7;48 am  /
         Driver.getDriver().findElement(By.cssSelector("[id*='search-query']")).sendKeys(search + Keys.ENTER);
     } // 8;42 update
 
-    @Then("^I should see the results$")
+    @Then("^I should see the results$") // 8;53 update
     public void i_should_see_the_results() throws Throwable {
         Thread.sleep(2000);
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("search"));
